af34ba2870f145689edbca33a36df5bec3bd7315
Seo JeongSeop <91553607+seo92js@users.noreply.github.com>
2023-10-24 09:41:33 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        
+        for(int i : numbers) {
+            answer+=i;
+            if (answer > n)
+                return answer;
+        }
+        
+        return answer;
+    }
+}
