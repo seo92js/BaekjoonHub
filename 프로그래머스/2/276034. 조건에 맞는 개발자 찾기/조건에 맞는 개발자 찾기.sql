ede0438e9c1b4858a0115cb9cfd1070a18596ffd
Seo JeongSeop <91553607+seo92js@users.noreply.github.com>
2024-04-03 01:13:39 +0900

    [level 2] Title: 조건에 맞는 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/276034. 조건에 맞는 개발자 찾기/조건에 맞는 개발자 찾기.sql
@@ -0,0 +1,6 @@
+SELECT id, email, first_name, last_name
+FROM developers
+WHERE skill_code & (SELECT code FROM skillcodes WHERE name = 'Python')
+OR
+skill_code & (SELECT code FROM skillcodes WHERE name = "C#")
+ORDER BY id ASC
