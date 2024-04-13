c9684303ebae75043befb5078650ce1b4ec40d3b
Seo JeongSeop <91553607+seo92js@users.noreply.github.com>
2024-04-13 18:05:29 +0900

    [level 0] Title: ad 제거하기, Time: 0.93 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        List<String> list = new ArrayList<>();
+        
+        for(String s : strArr) {
+            if(!s.contains("ad")) {
+                list.add(s);
+            }
+        }
+        
+        return list.toArray(new String[list.size()]);
+    }
+}
