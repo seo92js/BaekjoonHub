f0fffe7aabaa1e1c49ff89db697f5fd240913212
Seo JeongSeop <91553607+seo92js@users.noreply.github.com>
2024-04-15 15:50:21 +0900

    [level 0] Title: 배열 만들기 3, Time: 3.29 ms, Memory: 83 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i = 0; i < intervals.length; i++) {
+            for(int j = intervals[i][0]; j <= intervals[i][1]; j++) {
+                list.add(arr[j]);
+            }
+        }
+        
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
